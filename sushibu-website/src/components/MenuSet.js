a7000c385773a8bb0b3e5e8c3d480788766f124a
3Duona <titastitasbl@gmail.com>
2025-03-22 16:03:10 +0200

    Sushi set data update
    -Removed not needed photos
    +Fixed photos to represent correct sushi sets

--- sushibu-website/src/components/MenuSet.js
+++ sushibu-website/src/components/MenuSet.js
@@ -10,9 +10,8 @@ import iconVegan from "../content/Icons/Icon_Vegan.png";
 // import iconVeganDefault from "../content/Icons/Icon_VeganDefault.png";
 
 import sushiSet_1 from "../content/SushiSet/SushiSet_Bu1.jpg";
-import sushiSet_2 from "../content/SushiSet/SushiSet_Bu2.jpg";
 import sushiSet_3 from "../content/SushiSet/SushiSet_Bu3.jpg";
-import sushiSet_4 from "../content/SushiSet/SushiSet_Bu4.jpg";
+import sushiSet_6 from "../content/SushiSet/SushiSet_Bu6.jpg";
 import sushiSet_BigBu from "../content/SushiSet/SushiSet_BigBu.jpg";
 
 const MenuSet = () => {
@@ -39,7 +38,7 @@ const MenuSet = () => {
             name="BŪ-2; (16 vnt)"
             desc="Philadelphia 4 vnt, California 4 vnt, Aliaska 4 vnt, Sezame Maguro 4 vnt"
             price="18.00"
-            img={sushiSet_3}
+            img={sushiSet_BigBu}
           ></CreateCard>
         </Grid>
         <Grid item xs={12} md={6} l={4}>
@@ -47,7 +46,7 @@ const MenuSet = () => {
             name="BŪ-3; (16 vnt)"
             desc="Sake 4 vnt, Teka 4 vnt, Ebi 4 vnt, Kani 4 vnt"
             price="13.00"
-            img={sushiSet_2}
+            img={sushiSet_3}
           ></CreateCard>
         </Grid>
         <Grid item xs={12} md={6} l={4}>
@@ -55,7 +54,7 @@ const MenuSet = () => {
             name="BŪ-4; (16 vnt)"
             desc="Wakame 4 vnt, Avocado 4 vnt, Yasai 8 vnt"
             price="15.00"
-            img={sushiSet_4}
+            img={sushiSet_BigBu}
             icon={iconVegan}
           ></CreateCard>
         </Grid>
@@ -64,7 +63,7 @@ const MenuSet = () => {
             name="BŪ-5; (24 vnt)"
             desc="Sake Wakame 8 vnt, Sezame Maguro 8 vnt, Kani 8 vnt"
             price="23.00"
-            img={sushiSet_1}
+            img={sushiSet_BigBu}
           ></CreateCard>
         </Grid>
         <Grid item xs={12} md={6} l={4}>
@@ -72,7 +71,7 @@ const MenuSet = () => {
             name="BŪ-6; (24 vnt)"
             desc="Tataki Sake 8 vnt, Ebi Avocado 8 vnt, Ebi 8 vnt"
             price="25.00"
-            img={sushiSet_1}
+            img={sushiSet_6}
           ></CreateCard>
         </Grid>
         <Grid item xs={12} md={6} l={4}>
@@ -80,7 +79,7 @@ const MenuSet = () => {
             name="BŪ-7; (40 vnt)"
             desc="Sake 8 vnt, Wakame 8 vnt, California 8 vnt, Sake Wakame 8 vnt, Ebi Avocado 8 vnt"
             price="40.00"
-            img={sushiSet_1}
+            img={sushiSet_BigBu}
           ></CreateCard>
         </Grid>
         <Grid item xs={12} md={6} l={4}>
@@ -88,7 +87,7 @@ const MenuSet = () => {
             name="BŪ-8; (48 vnt)"
             desc="Ebi 4 vnt, Kani 4 vnt, Tekka 4 vnt, Sake 4 vnt, Philadelphia 8 vnt, California 8 vnt, Alaska 8 vnt, Sezame Maguro 8 vnt"
             price="50.00"
-            img={sushiSet_1}
+            img={sushiSet_BigBu}
           ></CreateCard>
         </Grid>
         <Grid item xs={12} md={6} l={4}>
@@ -96,7 +95,7 @@ const MenuSet = () => {
             name="BŪ Draugų; (48 vnt)"
             desc="(Alaska 8 vnt, California 8 vnt, Sake Wakame 8 vnt, Traškios Krevetės 4 vnt, Sūrio Spurgytės 4 vnt, Spring Rolls 4 vnt, Kalmarų Žiedai 4 vnt"
             price="50.00"
-            img={sushiSet_1}
+            img={sushiSet_BigBu}
           ></CreateCard>
         </Grid>
         <Grid item xs={12} md={6} l={4}>
